70e4fb423719646786f9d99ddbf41cc56de1f6c3
Ethan Dinh <98794440+ethan-dinh@users.noreply.github.com>
2022-07-08 16:40:29 -0700

    Update

--- scripts/scrollmagic.js
+++ scripts/scrollmagic.js
@@ -1,11 +1,9 @@
 $(function () { // wait for document ready
-      // init
+      // init controller
       var controller = new ScrollMagic.Controller({
             globalSceneOptions: {
                   triggerHook: 'onLeave',
-                  duration: "100%" // this works just fine with duration 0 as well
-                  // However with large numbers (>20) of pinned sections display errors can occur so every section should be unpinned once it's covered by the next section.
-                  // Normally 100% would work for this, but here 200% is used, as Panel 3 is shown for more than 100% of scrollheight due to the pause.
+                  duration: "100%"
             }
       });
 
@@ -21,6 +19,8 @@ $(function () { // wait for document ready
                   .addTo(controller);
       }
 });
+
+
 
 
     
